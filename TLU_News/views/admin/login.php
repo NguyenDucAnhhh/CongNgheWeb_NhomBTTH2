98a3ef7e4f1760386b7326c2af51627aa348ceb6
Duc Anh <ducanh1320040@gmail.com>
2024-12-03 13:08:14 +0700

    update

--- TLU_News/views/admin/login.php
+++ TLU_News/views/admin/login.php
@@ -1,27 +1,5 @@
 <?php
-    session_start();
-    ob_start();
-    include "../../db.php";
-    include "../../models/Users.php";
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['dangnhap'])) {
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-
-        $role = checkUsers($username, $password, $conn);
-        if ($role !== false) {
-            $_SESSION['role'] = $role;
-            if ($role == 1) {
-                header('Location: dashboard.php');
-                exit();
-            }elseif ($role == 0) {
-                header('Location: ../../controllers/AdminController.php');
-                exit();
-            }
-        } else {
-            $txt_error = "Tên đăng nhập hoặc mật khẩu không đúng.";
-        }
-    }
+    include "../../controllers/AdminController.php";
 ?>
 
 <!DOCTYPE html>
