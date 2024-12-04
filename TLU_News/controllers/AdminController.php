590d4304cb88db5dff3b6ea6d845db060825537c
Nguyễn Đức Anh <168102836+NguyenDucAnhhh@users.noreply.github.com>
2024-12-04 23:35:55 +0700

    fix bug

--- TLU_News/controllers/AdminController.php
+++ TLU_News/controllers/AdminController.php
@@ -1,48 +1,48 @@
 <?php
-    class AdminController{
-        function login(){
+class AdminController{
+    function login(){
 
-            $txt_error = "";
+        $txt_error = "";
 
-            ob_start();
-            require_once __DIR__ . '/../db.php';
-            require_once __DIR__ . '/../models/Users.php';
+        ob_start();
+        require_once __DIR__ . '/../db.php';
+        require_once __DIR__ . '/../models/Users.php';
 
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['dangnhap'])) {
-                $username = trim($_POST['username']);
-                $password = trim($_POST['password']);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['dangnhap'])) {
+            $username = trim($_POST['username']);
+            $password = trim($_POST['password']);
 
-                $role = checkUsers($username, $password, $conn);
+            $role = checkUsers($username, $password, $conn);
 
-            if ($role) {
-                    $_SESSION['role'] = $role;
+            if (isset($role)) {
+                $_SESSION['role'] = $role;
 
-                    if ($role == 1) {
-                    header('Location: views/admin/dashboard.php');
-                        exit();
-                    } elseif ($role == 0) {
-                        header('Location: ../../views/home/index.php');
-                        exit();
-                    }
+                if ($role == 1) {
+                    header('Location: /TLU_News/index.php?controller=Admin&action=dashboard');
+                    exit();
+                } elseif ($role == 0) {
+                    header('Location: /TLU_News/index.php?controller=Home&action=showListNew');
+                    exit();
+                }
             } else {
                 $txt_error = "Tên đăng nhập hoặc mật khẩu không đúng.";
-                }
-            }
-            
-            require 'views/admin/login.php';
-
-        }
-        function logout(){
-
-            if (isset($_GET['action']) && $_GET['action'] =='logout') {
-                session_unset();
-                session_destroy();
-                $this->login();
-                exit();
             }
         }
-        function dashboard(){
-            require 'views/admin/dashboard.php';
+
+        require 'views/admin/login.php';
+
+    }
+    function logout(){
+
+        if (isset($_GET['action']) && $_GET['action'] =='logout') {
+            session_unset();
+            session_destroy();
+            header ('Location: /TLU_News/index.php?controller=Admin&action=login');
+            exit();
         }
     }
+    function dashboard(){
+        require 'views/admin/dashboard.php';
+    }
+}
 ?>
